b4527928c9471afe9e3459b7533e0a1bdffd9f44
nsym <mickey-ya@outlook.jp>
2022-02-13 05:06:03 +0900

    update: templateの使用

--- main.go
+++ main.go
@@ -3,8 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
+	"path/filepath"
+	"html/template"
 )
 
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`
